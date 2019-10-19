9a1f0a1cedb0ce59e3cf584440c24805c31b8992
vapvin <tpehdj12@daum.net>
2019-10-19 16:32:19 +0900

    Complete

--- starnight/js/main.js
+++ starnight/js/main.js
@@ -2,12 +2,19 @@ const video = document.getElementById("myVideo");
 
 const btn = document.getElementById("play");
 
+const start = document.getElementById("angle");
+const mute = document.getElementById("mute");
+
 const myFunction = () => {
   if (video.paused) {
     video.play();
+    mute.style.display = "block";
+    start.style.display = "none";
   } else {
     video.pause();
+    mute.style.display = "none";
+    start.style.display = "block";
   }
 };
-
+myFunction();
 btn.addEventListener("click", myFunction);
