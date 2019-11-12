3026007a081a201e9bdb0d441466745d68800b9f
vapvin <tpehj12@daum.net>
2019-11-12 09:43:03 +0900

    Coding the LayOut

--- Alecto/js/main.js
+++ Alecto/js/main.js
@@ -1,47 +1,66 @@
 // Classes
 
-var SHOWING_CLASS = "showing";
+const SHOWING_CLASS = "showing";
+const CIRCLE_CLASS = "circles";
 
 //Elements
 
-var firstSlide = document.querySelector(".slide_items:first-child");
-var lastSlide = document.querySelector(".slide_items:last-child");
-var nextBtn = document.querySelector(".next");
-var prevBtn = document.querySelector(".prev");
+const firstSlide = document.querySelector(".slide_items:first-child");
+const lastSlide = document.querySelector(".slide_items:last-child");
+const firstCircle = document.querySelector(".circle:first-child");
+const lastCircle = document.querySelector(".circle:last-child");
+const nextBtn = document.querySelector(".next");
+const prevBtn = document.querySelector(".prev");
 
 // Functions
 
 function nextSlides() {
-  var currentSlide = document.querySelector(`.${SHOWING_CLASS}`);
+  const currentSlide = document.querySelector(`.${SHOWING_CLASS}`);
+  const currentCircle = document.querySelector(`.${CIRCLE_CLASS}`);
 
   if (currentSlide) {
     currentSlide.classList.remove(SHOWING_CLASS);
-    var nextSlide = currentSlide.nextElementSibling;
+    currentCircle.classList.remove(CIRCLE_CLASS);
+
+    const nextSlide = currentSlide.nextElementSibling;
+    const nextCircle = currentCircle.nextElementSibling;
 
     if (nextSlide) {
       nextSlide.classList.add(SHOWING_CLASS);
+      nextCircle.classList.add(CIRCLE_CLASS);
+
     } else {
       firstSlide.classList.add(SHOWING_CLASS);
+      firstCircle.classList.add(CIRCLE_CLASS);
+
     }
   } else {
     firstSlide.classList.add(SHOWING_CLASS);
+    firstCircle.classList.add(CIRCLE_CLASS);
   }
 }
 
 function prevSlides() {
-  var currentSlide = document.querySelector(`.${SHOWING_CLASS}`);
+  const currentSlide = document.querySelector(`.${SHOWING_CLASS}`);
+  const currentCircle = document.querySelector(`.${CIRCLE_CLASS}`);
+
 
   if (currentSlide) {
     currentSlide.classList.remove(SHOWING_CLASS);
-    var prevSlide = currentSlide.previousElementSibling;
+    currentCircle.classList.remove(CIRCLE_CLASS);
+    const prevSlide = currentSlide.previousElementSibling;
+    const prevCircle = currentCircle.previousElementSibling;
 
     if (prevSlide) {
       prevSlide.classList.add(SHOWING_CLASS);
+      prevCircle.classList.add(CIRCLE_CLASS);
     } else {
       lastSlide.classList.add(SHOWING_CLASS);
+      lastCircle.classList.add(CIRCLE_CLASS);
     }
   } else {
     lastSlide.classList.add(SHOWING_CLASS);
+    lastCircle.classList.add(CIRCLE_CLASS);
   }
 }
 
